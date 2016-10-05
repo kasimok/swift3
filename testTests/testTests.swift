c4a415b3b7b41744ee765986062078c75cdb3bc5
Evilisn <evilisn_jiang@keroro-gunso.local>
2016-10-05 22:16:02 +0800

    this is for start

--- testTests/testTests.swift
+++ testTests/testTests.swift
@@ -13,6 +13,7 @@ class testTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        print("Starting Test");
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -24,6 +25,7 @@ class testTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        super.
     }
     
     func testPerformanceExample() {
